260149218925d3e3b18c62bcdf110cd38b83284d
kolodxxv <crowcrxst@gmail.com>
2024-06-18 15:54:28 +0300

    Test cases and routing

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title></title>
+</head>
+<body>
+    <h1>Birdboard</h1>
+
+    <ul>
+        @foreach ($projects as $project)
+            <li>{{ $project->title }}</li>
+        @endforeach
+    </ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProjectsController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/projects', [ProjectsController::class, 'index']);
+
+Route::post('/projects', [ProjectsController::class, 'store']);
